3d59812f2e6c04ecaee0391bb6dcea9f0ab4e50d
petrsjan <64227275+petrsjan@users.noreply.github.com>
2025-05-03 08:55:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+function openProject(element) {
+  const title = element.dataset.title;
+  const author = element.dataset.author;
+  const year = element.dataset.year;
+  const place = element.dataset.place;
+  const description = element.dataset.description;
+  const imageSrc = element.querySelector('img').src;
 
-function openProject(id) {
+  document.getElementById('detail-title').innerText = title;
+  document.getElementById('detail-author').innerText = author;
+  document.getElementById('detail-year').innerText = year;
+  document.getElementById('detail-place').innerText = place;
+  document.getElementById('detail-description').innerText = description;
+  document.querySelector('.detail-image img').src = imageSrc;
+
+  const thumbnails = document.querySelectorAll('.thumbnail-bar img');
+  thumbnails.forEach(thumb => {
+    thumb.src = imageSrc;
+  });
+
   document.getElementById('project-detail').classList.remove('hidden');
   window.scrollTo(0, 0);
 }
@@ -8,6 +26,7 @@ function closeProject() {
   document.getElementById('project-detail').classList.add('hidden');
 }
 
-function showImage(src) {
-  document.querySelector('.main-image').src = src;
+function zoomThumbnail(el) {
+  const mainImg = document.querySelector('.detail-image img');
+  mainImg.src = el.src;
 }
